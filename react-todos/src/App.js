4d3cddbd8b9651e72ce4475193b67fccf4a349a1
Brad Lim <brad.dh.lim@gmail.com>
2023-12-16 23:54:12 -0800

    Add conditional rendering

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -5,6 +5,8 @@ import NewTodoForm from './components/NewTodoForm';
 
 function App() {
 
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
   const [todos, setTodos] = useState([
     {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
     {rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
@@ -16,14 +18,17 @@ function App() {
   const addTodo = (description, assigned) => {
     let rowNumber = 0
     if (todos.length > 0) {
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
+      rowNumber = 1;
+    }
       const newTodo = {
-        rowNumber: todos.length + 1,
+        rowNumber: rowNumber,
         rowDescription: description,
         rowAssigned: assigned 
       };
       setTodos(todos => [...todos, newTodo])
     }
-  }
 
   const deleteTodo = (deleteTodoRowNumber) => {
     let filtered = todos.filter(function (value) {
@@ -40,10 +45,12 @@ function App() {
         </div>
         <div className="card-body">
           <TodoTable todos={todos} deleteTodo={deleteTodo}/>
-          <button className="btn btn-primary" onClick={addTodo}>
-            Add new todo
+          <button onClick={() => setShowAddTodoForm(!showAddTodoForm)} className="btn btn-primary">
+            {showAddTodoForm ? 'Close New Todo' : 'New Todo'}
           </button>
+        {showAddTodoForm &&
           <NewTodoForm addTodo={addTodo}/>
+        }
         </div>
       </div>
     </div>
